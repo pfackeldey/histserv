ef6301b0ed6b30708b1817e06d7a7577c19d64ac
pfackeldey <fackeldey.peter@gmail.com>
2026-03-31 16:01:34 +0200

    bump version

--- src/histserv/__init__.py
+++ src/histserv/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 __author__ = "Peter Fackeldey"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __all__ = ["ChunkedHist", "Client", "RemoteHist", "Server", "ServerOptions"]
 
